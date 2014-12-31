e6e3202a7c04493559d82f7f9aa4970b90e67597
elvizcacho <elvizcacho@gmail.com>
2014-12-31 12:33:46 -0500

    initilization done

--- db/migrate/20141231171743_initialization.rb
+++ db/migrate/20141231171743_initialization.rb
@@ -2,9 +2,9 @@ class Initialization < ActiveRecord::Migration
   def change
   	Role.create(:id => 1, :name => "admin")
   	ControllerAction.create(:id => 1, :name => "users")
-  	ControllerAction.create(:id => 2, :name => "index", controller_action_id: => 1)
-  	ControllerAction.create(:id => 3, :name => "create", controller_action_id: => 1)
-  	ControllerAction.create(:id => 4, :name => "destroy", controller_action_id: => 1)
+  	ControllerAction.create(:id => 2, :name => "index", :controller_action_id => 1)
+  	ControllerAction.create(:id => 3, :name => "create", :controller_action_id => 1)
+  	ControllerAction.create(:id => 4, :name => "destroy", :controller_action_id => 1)
   	admin = Role.find(1)
   	users = ControllerAction.find(1)
   	admin.controller_actions << users
