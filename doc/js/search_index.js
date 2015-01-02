bf0600016be32c84f303cbd4c557a254a9b87fd9
Sebastian Vizcaino <elvizcacho@gmail.com>
2015-01-02 15:56:10 -0500

    show documentation

--- doc/js/search_index.js
+++
@@ -1,1 +0,0 @@
-var search_data = {"index":{"searchIndex":["readme"],"longSearchIndex":[""],"info":[["README","","README_rdoc.html","","<p>README\n<p>This README would normally document whatever steps are necessary to get the\napplication up and …\n"]]}}
