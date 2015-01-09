e9651bff840e2cb561b382c4ae3808c1c8dff9fa
elvizcacho <elvizcacho@gmail.com>
2015-01-09 17:04:17 -0500

    documentation roles#permissions

--- app/controllers/api/v1/roles_controller.rb
+++ app/controllers/api/v1/roles_controller.rb
@@ -161,7 +161,9 @@ module Api
 
         ##
         # Gets Role permissions.
+        #
         # It returns an array of all id actions and permissions into them.
+        #
         # GET /api/v1/roles/:id/permissions
         #
         # params:
